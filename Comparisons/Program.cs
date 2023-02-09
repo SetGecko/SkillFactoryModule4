b8d7a10e75b468bb85501c54f9a69ffaf7404c32
Ипполитов Алексей <setgecko@gmail.com>
2023-02-09 20:56:35 +0300

    логические операторы практика 4.1

--- Comparisons/Program.cs
+++ Comparisons/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            /*
+             *  == 	Равенство
+             *  <  	Меньше чем
+             *  <= 	Меньше или равно
+             *  != 	Неравенство
+             *  >  	Больше чем
+             *  >= 	Больше или равно
+            */
             var myapples = 5;
             var hisapples = 6;
             var hispeares = 5;
@@ -11,6 +19,28 @@
             var result = (myapples != hispeares) & (myapples < hisapples);
 
             Console.WriteLine(result);
+            Console.WriteLine("---");
+            Console.WriteLine("---");
+            Console.WriteLine("---");
+
+            /*
+             * |   или
+             * ||  или
+             * &&   и
+             * &    и
+             * !    не
+            */
+
+            int currentDayInYear = 293;
+            bool isLeapyear = false;
+
+            bool isWinter =
+                (!isLeapyear & (currentDayInYear >= 335 | currentDayInYear <= 59))
+                |
+                (isLeapyear & (currentDayInYear >= 336 | currentDayInYear <= 60));
+
+            Console.WriteLine(isWinter);
+
             Console.ReadKey();
         }
     }
