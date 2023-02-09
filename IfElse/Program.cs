8f4d658c3dd7f096efe27227a9f72ef317ad5566
Ипполитов Алексей <setgecko@gmail.com>
2023-02-09 22:04:42 +0300

    Выполнил задание 4.1.12

---
+++ IfElse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 7;
+            var b = 7;
+
+            if (a == b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+        }
+    }
+}
---
+++ Task_4.1.10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_4._1._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inv = true;
+            var result = !inv;
+
+            Console.WriteLine(result);
+        }
+    }
+}
