bd4de6424a21bf6298b61c8cc0f6347b086139da
Ипполитов Алексей <setgecko@gmail.com>
2023-02-09 21:08:44 +0300

    Выполнил задание 4.1.4

---
+++ Task_4.1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_4._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A;
+            string B;
+            bool C = A != B;
+        }
+    }
+}
