01c82c25b7f6baaf6d582e6695bd8c51a5ee8af3
Ипполитов Алексей <setgecko@gmail.com>
2023-02-09 21:11:29 +0300

    Выполнил задание 4.1.5

---
+++ Task_4.1.5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_4._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            double X;
+            double Y;
+
+            bool C = (A < B) | (X > Y);
+        }
+    }
+}
