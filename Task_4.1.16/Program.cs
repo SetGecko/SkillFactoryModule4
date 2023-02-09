68b5d740f99157a9ac8af3907e848ae12924d944
Ипполитов Алексей <setgecko@gmail.com>
2023-02-09 22:26:12 +0300

    Практика по 4.1.16 + по 4.1.17 заливаю в репозиторий

---
+++ Task_4.1.16/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_4._1._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на наглийском с маленькой буквы.");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor= ConsoleColor.Red;
+                Console.ForegroundColor= ConsoleColor.Black;
+
+                Console.WriteLine("Ваш цвет красный!");
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Ваш цвет зелёный!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Ваш цвет голубой!");
+            }
+        }
+    }
+}
