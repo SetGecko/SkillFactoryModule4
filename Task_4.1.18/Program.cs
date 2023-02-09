470fdb9cfda7b81daf7b74f514e0cdf3358069c1
Ипполитов Алексей <setgecko@gmail.com>
2023-02-09 22:36:28 +0300

    Выполнил задание 4.1.18

---
+++ Task_4.1.18/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_4._1._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на наглийском с маленькой буквы.");
+
+            var color = Console.ReadLine();
+
+            switch(color) 
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor= ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш цвет красный!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш цвет зелёный!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш цвет голубой!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Ваш цвет жёлтый!");
+                    break;
+            }
+        }
+    }
+}
