a7f44cff7e9c89cd74bf360fe06e6536795d5d02
Ипполитов Алексей <setgecko@gmail.com>
2023-02-09 22:17:55 +0300

    Практика и разбор по тернарным операциям

---
+++ TernaryOperation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TernaryOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 6;
+            var b = 7;
+
+            var c = a != b ? a + b : b;
+
+            Console.WriteLine(c);
+
+            // c = 
+            // если а не равно b
+            // после ? (если истинно)  = a + b
+            // после : (если ложно) = b
+        }
+    }
+}
