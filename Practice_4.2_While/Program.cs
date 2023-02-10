442991d8228aa7bcb81e3b352329cc9b7177d927
Ипполитов Алексей <setgecko@gmail.com>
2023-02-10 10:19:43 +0300

    Практика while тема 4.2

---
+++ Practice_4.2_While/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice_4._2_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 5)
+            {
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("До входа в цикл i = {0}", i);
+                Console.WriteLine("Бэлла");
+                i++;
+                Console.WriteLine("Сейчас i = {0}", i);
+                Console.WriteLine("------------------------------");
+            }
+        }
+    }
+}
