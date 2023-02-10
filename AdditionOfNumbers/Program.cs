2e48a198abb59c38849ebe5a95a1dd2fbcfde38e
Ипполитов Алексей <setgecko@gmail.com>
2023-02-10 20:34:25 +0300

    Практика по скринкасту "Сложение чисел"

---
+++ AdditionOfNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AdditionOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                var number = Convert.ToInt32(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    continue;
+                }
+                else if (number == 0)
+                {
+                    break;
+                }
+                sum = sum + number;
+            }
+
+            Console.WriteLine("Итоговая сумма: {0}", sum);
+        }
+    }
+}
---
+++ Task_4.2.11/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task_4._2._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Цикл Do");
+
+            int t = 0;
+
+            do
+            {
+                Console.WriteLine(t);
+                Console.WriteLine("Напишите свой любимый цвет на англисйком с маленькой буквы");
+
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет красный!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет зелёный!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет голубой!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Ваш цвет жёлтый!");
+                        break;
+                } t++;
+            } while (t < 3);
+
+    }
+    }
+}
