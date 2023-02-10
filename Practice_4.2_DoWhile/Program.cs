da9620110c771a566956514d09b41162ffb115dc
Ипполитов Алексей <setgecko@gmail.com>
2023-02-10 10:28:52 +0300

    Практика 4.2 Do While

---
+++ Practice_4.2_DoWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice_4._2_DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("До входа в цикл i = {0}", i);
+                Console.WriteLine("Бэлла");
+                i++;
+                Console.WriteLine("Сейчас i = {0}", i);
+                Console.WriteLine("------------------------------");
+            }
+            while (i < 5);
+        }
+    }
+}
