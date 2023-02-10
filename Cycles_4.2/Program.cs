a0574537b79e42c41754db3d60099ec4989aec21
Ипполитов Алексей <setgecko@gmail.com>
2023-02-10 09:41:52 +0300

    Практика 4.2 - добавил внутрь for условия из 4.1.18

---
+++ Cycles_4.2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Cycles_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на наглийском с маленькой буквы.");
+
+            //var color = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Итерация {0}", i);
+
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет красный!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет зелёный!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет голубой!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Ваш цвет жёлтый!");
+                        break;
+                }
+            }
+        }
+    }
+}
