4e69cff9b484cf58a30c442977fbf77d768f300d
Ипполитов Алексей <setgecko@gmail.com>
2023-02-10 10:07:32 +0300

    Практика по созданию простого цикла

---
+++ Practice_4.2_For/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice_4._2_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine("Бэлла");
+                Console.WriteLine("Это было написано {0} раз", i);
+            }
+        }
+    }
+}
