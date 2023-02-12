da680014c8d39cb62a02447ecbb40b0e79c1acf0
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 00:09:51 +0300

    Прервёл окрашивалку текста в массив Практика по массивам с пеербором

---
+++ Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favcolors = new string[3];
+
+            for (int i = 0; i < favcolors.Length; i++) 
+            {
+                Console.WriteLine("Введите люимый цвет номер {0}", i + 1);
+                favcolors[i] = Console.ReadLine();
+            }
+
+            foreach (var color in favcolors)
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет красный!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет зелёный!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш цвет голубой!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Ваш цвет жёлтый!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ PracticeArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PracticeArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Сделал тестовый массив randomNumbers
+             * Дал значения элементам массива
+             * Перечислил все значения массива с указанием их номера
+             * Примеры инициализация массива:
+             * 		int[] arr1 = new int[4] { 1,2,3,5};
+		            int[] arr2 = new int[] { 1,2,3,5};
+		            int[] arr3 = new[] { 1,2,3,5};
+		            int[] arr4 = { 1,2,3,5};
+             */
+            int[] randomNumbers = new int[2];
+            randomNumbers[0] = 1;
+            randomNumbers[1] = 4;
+
+            for (int i = 0; i < randomNumbers.Length;i++) 
+            {
+                Console.WriteLine("{0} значение мыссива = {1}", i+1, randomNumbers[i]);
+            }
+        }
+    }
+}
