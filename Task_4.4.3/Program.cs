660668d4b6b718846522c1431529e684605cdef4
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 05:23:15 +0300

    Выполнил задание 4.4.3

---
+++ Task_4.4.3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_4._4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var (name, age) = ("Алексей", 35);
+
+            Console.WriteLine("Моё имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+
+
+            Console.Write("Введите  имя: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст с цифрами: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+        }
+    }
+}
