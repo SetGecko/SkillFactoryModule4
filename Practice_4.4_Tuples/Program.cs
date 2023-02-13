cbc8b7429c7d858cd94f1788e77996b421d8f670
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 05:15:09 +0300

    Выполнил задание 4.4.2

---
+++ Practice_4.4_Tuples/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice_4._4_Tuples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var anketa = (name: "Алексей", age : 35);
+            // а можно так:
+            // (string name, int age) anketa;
+            // var (name, age) = ("Евгения", 27); - так только если сразу вводим значения кортежа
+
+            Console.WriteLine("Ваше имя: {0}",anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
+            Console.WriteLine("Ваше имя: {0}", anketa.Item1);
+            Console.WriteLine("Ваш возраст: {0}", anketa.Item2);
+        }
+    }
+}
---
+++ Task_4.4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace Task_4._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, int age) anketa;
+
+            Console.WriteLine("Введите ваше имя:");
+            anketa.name = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш возраст:");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+        }
+    }
+}
