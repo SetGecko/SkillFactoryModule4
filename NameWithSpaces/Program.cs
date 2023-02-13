aa358787b213ea6f3c1f03fe6217baac55475e68
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 03:14:12 +0300

    Практика программа Имя с пробелами

---
+++ NameWithSpaces/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NameWithSpaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по буквам:");
+
+            foreach(var letter in name)
+            {
+                Console.Write(letter + " ");
+            }
+
+            Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+        }
+    }
+}
