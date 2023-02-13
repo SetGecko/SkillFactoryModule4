dec19c5446a914dc8401c797dd6484244d0de4b4
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 05:29:17 +0300

    Выполнил задание 4.4.5

---
+++ Task_4.4.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+
+namespace Task_4._4._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age, int NameCount) Pet;
+
+            Console.Write("Введите имя питомца: ");
+            Pet.Name = Console.ReadLine();
+            Pet.NameCount = Pet.Name.Length;
+
+            Console.Write("Введите тип питомца: ");
+            Pet.Type = Console.ReadLine();
+
+            Console.Write("Введите возраст питомца цифрой: ");
+            Pet.Age = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
