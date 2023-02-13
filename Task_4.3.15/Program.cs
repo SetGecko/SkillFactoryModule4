487476de5ded7ded71d382936b721121c29ff087
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 04:35:48 +0300

    Практика 4.3.15

---
+++ Task_4.3.15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_4._3._15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, -5, 6, 7, 8, 9, -8, 11, 0, -13};
+
+            var numbers = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                    numbers++;
+            }
+
+            Console.WriteLine(numbers);
+        }
+    }
+}
