8e8d427ab8770313a8da228fe16ef72aa38689a0
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 04:48:45 +0300

    Практика 4.3.16

---
+++ Task_4.3.16/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_4._3._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            int temp = 0;
+
+            foreach (var item in arr)
+            {
+                if (item > 0)
+                {
+                    temp++;
+                }
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
