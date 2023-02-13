11f59587a7185f7e48f3685390042d33dbbc19af
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 05:48:56 +0300

    Выполнил финальное задание 4.5

---
+++ PracticalWork_4.5/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace PracticalWork_4._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+
+            for(k = 0; k < 3; k++)
+            {
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+                User.LastName = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                switch (Console.ReadLine())
+                {
+                    case "Да":
+                        User.HasPet = true;
+                        break;
+                    case "Нет":
+                        User.HasPet = false;
+                        break;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+                User.Age = Convert.ToInt32(Console.ReadLine());
+
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
