3b3847e919ac5acdde9df24118421fa381e31fb1
Ипполитов Алексей <setgecko@gmail.com>
2023-02-13 03:16:16 +0300

    Выполнено упражнение 4.3.7

---
+++ Task_4.3.7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_4._3._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя:");
+            string name = Console.ReadLine();
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i] + " ");
+            }
+        }
+    }
+}
